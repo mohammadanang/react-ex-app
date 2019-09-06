af36ef32a97e637e8b708e774e5a8d9b79959898
mohammadanang <m.anangnur@gmail.com>
2019-09-06 10:33:48 +0700

    add some package for development

--- src/index.js
+++ src/index.js
@@ -3,8 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { createBrowserHistory } from "history";
+import { Router, Route, Switch, Redirect } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const hist = createBrowserHistory();
+
+ReactDOM.render(
+    <>
+        <App />
+        <ToastContainer position="top-center" autoClose={4000} hideProgressBar={true} />
+    </>, 
+    document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
